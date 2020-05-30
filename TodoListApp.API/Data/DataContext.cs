bbac3a8f5654533d7bace8e75dc127ef1bde1746
Kyle Bush <kylebush1986@gmail.com>
2020-05-30 14:53:15 -0600

    Initial commit

---
+++ TodoListApp.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using TodoListApp.API.Models;
+
+namespace TodoListApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+        public DbSet<Value> Values { get; set; }
+    }
+}
