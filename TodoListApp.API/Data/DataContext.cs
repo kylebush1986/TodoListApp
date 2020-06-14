94b7450b59dede5792ea55a03c24b4b3729832f4
Kyle Bush <kylebush1986@gmail.com>
2020-06-14 15:56:51 -0600

    Added register and login

--- TodoListApp.API/Data/DataContext.cs
+++ TodoListApp.API/Data/DataContext.cs
@@ -7,5 +7,6 @@ namespace TodoListApp.API.Data
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) {}
         public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
